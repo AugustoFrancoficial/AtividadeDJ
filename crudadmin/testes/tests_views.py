87ff7ea310da812a3caa68adb462abe799c89bf0
Augusto Franco <augusto890.nf@gmail.com>
2023-04-24 23:12:25 -0300

    primeiro commit

--- crudadmin/testes/tests_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-
-class MyViewTestCase(TestCase):
-    def test_my_view(self):
-        response = self.client.get(reverse('my_view'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Texto que deve estar na página")
--- ex/tests.py
+++ ex/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class MyViewTestCase(TestCase):
+    def test_my_view(self):
+        response = self.client.get(reverse('my_view'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Texto que deve estar na página")
